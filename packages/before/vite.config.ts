464183e8007c01e21446701e861044121dcbdc78
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-22 23:12:09 +0900

    chore: shadcn ui 설치

--- packages/before/vite.config.ts
+++ packages/before/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
@@ -16,4 +16,4 @@ export default defineConfig({
     setupFiles: './src/test/setup.ts',
     css: true,
   },
-})
+});
