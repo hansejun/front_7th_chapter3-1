37832d94dce156e9b5d9157789b80863eb924f11
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-24 00:52:46 +0900

    feat: delete post 구현

--- packages/after/src/features/post/delete-post/use-delete-post.hook.ts
+++ packages/after/src/features/post/delete-post/use-delete-post.hook.ts
@@ -1,2 +1,22 @@
-// TODO: 구현 예정
-export const useDeletePost = () => {};
+import { postService } from '@/entities/post';
+import { usePosts } from '@/entities/post/use-posts.hook';
+import { useMutation } from '@/shared/hooks';
+
+export const useDeletePost = () => {
+  const { refetch: refetchPosts } = usePosts();
+
+  const { mutate } = useMutation(postService.delete);
+
+  const onDeletePost = (id: number) => {
+    mutate(id, {
+      onSuccess: () => {
+        refetchPosts();
+        // 1. 성공 알럿 보여주기 ('삭제되었습니다.')
+      },
+      onError: error => {
+        // 2. 에러 알럿 보여주기 (error.message || '삭제에 실패했습니다.')
+      },
+    });
+  };
+  return { onDeletePost };
+};
